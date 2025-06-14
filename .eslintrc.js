ebfe34e4ed6a3b475c850dc66f613b3a2f3fda35
Charlie <collon40408@gmail.com>
2025-06-14 18:49:27 +0800

    Add configuration files for project setup including .editorconfig, .eslintrc.js, .nvmrc, .prettierrc, and initial documentation files (CHANGELOG, CONTRIBUTING, LICENSE, README) for AI Feedback SDK.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier', // Add prettier to the end to override other configs
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // You can add or override ESLint rules here
+    // e.g., 'no-console': 'warn' to warn about console logs instead of failing
+  },
+}; 
